e394f1f6215a24fb47fae9e068d541695b74c767
Paulajungaker <paula010514@hotmail.com>
2025-07-15 13:45:57 +0200

    Styling navbar responsively

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AppRoutes from "AppRoutes";
+import "./index.css";
 
 export const App = () => {
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,15 @@ import "./styling/Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1 className="logotype">Femine Essence</h1>
+      <div className="nav-group">
+        <h2 className="logotype">Femine Essence</h2>
+        <ul className="nav-links">
+          <li>Home</li>
+          <li>Courses</li>
+          <li>Incense</li>
+          <li>Join us</li>
+        </ul>
+      </div>
       <img
         className="nav-icon"
         src="/icons/icon-hamburger.svg"
